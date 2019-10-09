89dd723ff19db33107af3656dc4c4409c8f7e4d3
Jannik Steck <Jannik Steck>
2019-10-09 10:53:15 +0200

    Zweiter Commit, Hilfe Button für die Werte wurden erstellt + Toast, was als Hilfe zu sehen ist + KOMMENTARE. gehen mit Shift+//. Rest werde ich dir an der Uni erklären

--- app/src/main/java/com/example/waagrechterwurf/MainActivity.java
+++ app/src/main/java/com/example/waagrechterwurf/MainActivity.java
@@ -2,22 +2,24 @@ package com.example.waagrechterwurf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
 
-    protected Button _weite_wird_berechnet;
-    protected Button _zur_hilfe_seite;
+    protected Button _weite_wird_berechnet = null;
+    protected Button _zur_hilfe_seite = null;
 
-    protected EditText _eingegebene_höhe;
-    protected EditText _eingegebene_beschleunigung;
+    protected EditText _eingegebene_höhe = null;
+    protected EditText _eingegebene_beschleunigung = null;
 
-    int hoehe;
-    int beschleunigung;
+    int hoehe = 0;
+    int beschleunigung = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +36,35 @@ public class MainActivity extends AppCompatActivity{
             public void onClick(View view) {
                 hoehe =  Integer.valueOf(_eingegebene_höhe.getText().toString());
                 beschleunigung = Integer.valueOf(_eingegebene_beschleunigung.getText().toString());
+
+
+            }
+        });
+// Toast erstellen für Geschwindigkeit (Hilfe)
+        Button hilfe_v = (Button) findViewById(R.id.hilfe_button_v);
+        hilfe_v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                Toast toast = Toast.makeText(context, getString(R.string.toast_hilfe_v), Toast.LENGTH_LONG);
+                toast.show();
             }
         });
 
+// Toast erstellen für Geschwindigkeit (Hilfe)
+        Button hilfe_h = (Button) findViewById(R.id.hilfe_button_h);
+        hilfe_h.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                Toast toast = Toast.makeText(context, getString(R.string.toast_hilfe_h), Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+
     }
+
+
 
 }
